52d8b1fe173a432ae9704bffec51466147ec474f
mark <gaurecasm@gmail.com>
2020-05-20 00:08:43 +0530

    added contact web development and graphics page

--- blackcode/src/app/home/home-routing.module.ts
+++ blackcode/src/app/home/home-routing.module.ts
@@ -2,8 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home.component';
+import { DevelopmentComponent } from './development/development.component';
+import { GraphicsComponent } from './graphics/graphics.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'development', component: DevelopmentComponent },
+  { path: 'graphics', component: GraphicsComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- blackcode/src/app/home/home.module.ts
+++ blackcode/src/app/home/home.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { DevelopmentComponent } from './development/development.component';
+import { GraphicsComponent } from './graphics/graphics.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, DevelopmentComponent, GraphicsComponent, ContactComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
